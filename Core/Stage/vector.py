28553464905e5804d8ca785a3c096e3f3124b118
Xyfuture <1728027202@qq.com>
2022-04-11 11:16:52 +0800

    change vector logic

--- Core/Stage/vector.py
+++ Core/Stage/vector.py
@@ -8,74 +8,71 @@ class Vector(StageBase):
         super(Vector, self).__init__()
 
         self.reg_file = reg_file
+
         self.vvset_length = 0
         self.vvset_bitwidth = 0
 
         self.current_eu = None
-        self.first_response = False
-        self.stalled = False
-        self.busy_cycles = 0
 
-        self.state = 'idle'
-        self.receivable = True # 可以接收新信息，这个主要是为了解决一个很别扭的问题
+        # 这几个是内部的寄存器，时钟下沿进行更新
+        self.stalled = False # stall_out 控制
+        self.busy_cycles = 0 # 需要额外等的周期数，本身已经占了一个周期了，除了本周期还需要多少个周期
+        self.state = 'idle'  # 表示下一个周期的情况，给update,stall_out使用的
+
 
 
     def ticktock(self):
 
-        if self.busy_cycles <= 1:
-            self.receivable = True
-        else:
-            self.receivable = False
-
         self.add_cycle_cnt()
         self.compute_cycle_energy()
 
-        self.first_response = False
+        if self.state == 'idle':
+            if self.current_eu == 'veu':
+                self.set_busy_cycle()
+                if self.busy_cycles > 0: # 如果为0，则不需要设置了
+                    self.state = 'busy'
+                return
 
+        if self.state == 'busy':
+            if self.busy_cycles - 1 > 0:
+                self.state = 'busy'
+            else:
+                self.state = 'idle'
+            self.busy_cycles -= 1
+            return
 
     def update(self):
-        eu,inst = self.recv_data['eu'],self.recv_data['inst']
-
-        # 正在处理中,更改busy_cycle等信息
-        if self.busy_cycles > 0:
-            self.busy_cycles -= 1
-            if self.busy_cycles == 0:
-                self.state = 'idle'
-
-
-        # 没在处理，正好是该部件的指令
-        if self.receivable and eu == 'veu':
-            self.first_response = True
-            self.state = 'busy'
-
-            self.stage_data = inst
-            self.set_busy_cycle()
-
-
-
+        if self.check_not_stalled():
+            # idle 模式，说明该部件当前没有在处理指令
+            if self.state == 'idle':
+                self.current_eu,self.stage_data = self.recv_data['eu'],self.recv_data['inst']
+        # 其他模式都不会更新stage_data
 
     def stall_out(self):
+        # 这里有个不太好的地方，其实不应该把stalled这个寄存器更新放在这里，应该放在ticktock中，因为他们在一起更新
 
-        # 最后一个周期，同时暂停了其他的部件
-        if self.busy_cycles == 1 and self.stalled:
-            self.stalled = False
-            return StallEvent('VectorExecuteUnit',False)
+        # 目前没有stall其他部件，检测是否需要stall其他部件
+        if not self.stalled:
+            # 下个周期是否忙，如果忙那么要对bypass过来的数据处理，确保下一周期不会发生数据的传递
+            if self.state == 'busy':
+                bypass_info = self.bypass_pre_stage_list[0].bypass_ticktock()
+                eu,inst = bypass_info['eu'],bypass_info['inst']
+                if eu == 'veu':
+                    self.stalled = True
+                    return StallEvent("VectorExecuteUnit",True)
 
-        # 正在处理中(不马山结束),且没有暂停其他的部件
-        if self.busy_cycles>1 and not self.stalled:
-            issue_info = self.bypass_pre_stage_list[0].bypass_ticktock()
-            eu,inst = issue_info['eu'],issue_info['inst']
+        # 目前已经阻塞了其他的部件
+        if self.stalled:
+            # 下个周期不忙，取消stall
+            if self.state == 'idle':
+                self.stalled = False
+                return StallEvent('VectorExecuteUnit',False)
 
-            if eu == 'veu':
-                self.stalled = True
-                return StallEvent('VectorExecuteUnit',True)
 
-        return None
 
 
     def compute_cycle_energy(self):
         pass
-
 
     def set_busy_cycle(self):
         pass
