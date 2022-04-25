c4a00da0cb1465591c940047506afe2d4c24ccd1
Xyfuture <1728027202@qq.com>
2022-04-25 11:12:28 +0800

    stall support

--- Core/Utils/misc.py
+++ Core/Utils/misc.py
@@ -2,13 +2,14 @@ from Core.Instruction.inst import instruction
 
 
 class ExecInfo:
-    def __init__(self,eu,inst:instruction,rd_value=0,rs1_value=0,rs2_value=0,length=0):
+    def __init__(self,eu,inst:instruction,rd_value=0,rs1_value=0,rs2_value=0,length=0,start_addr=None):
         self.eu = eu
         self.inst = inst
         self.rd_value = rd_value
         self.rs1_value = rs1_value
         self.rs2_value = rs2_value
         self.length = length
+        self.write_start_addr = start_addr
 
 
     def __getitem__(self, item):
